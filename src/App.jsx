75a47df535809bc8a6b6bf910f0ac75a2bf9908b
AniruthRamesh <anikrishna3@gmail.com>
2023-04-15 01:25:14 -0400

    finished form v1

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+import "./App.css"
+import { person, mail, call, logoUsd, helpCircle } from 'ionicons/icons';
+import { TypeAnimation } from 'react-type-animation';
+import { useState} from 'react';
+import axios from "axios"
+
+
+function App() {
+  const [name,setName] = useState("")
+  const [email,setEmail] = useState("")
+  const [mobileNumber,setMobileNumber] = useState("")
+  const [gig,setGig] = useState("")
+  const [cost,setCost] = useState("")
+
+  const handleSubmit = async (e)=>{
+    e.preventDefault();
+    console.log(name,email,mobileNumber,gig,cost)
+    
+    
+    const response = await axios.post("http://localhost:4000/api/gigs",{Name:name,emailid:email,mobileno:mobileNumber,gig:gig,cost:cost})
+    console.log(response)
+  }
+
+
+  return (
+    <div className="bg-dark background-dark">
+      
+      <h3 className="text-light sticky-top font" style={{ zIndex: 999 }}>
+      <TypeAnimation 
+      sequence={[
+        'HuskyHive.',
+        3000,
+        ''
+        
+      ]}
+      speed = {1}
+      wrapper="span"
+      cursor={true}
+      repeat={Infinity}
+      loop={true}  
+      style={{  display: 'inline-block'}}
+      className="text-light"
+    />
+      </h3>
+      <div style={{backgroundColor:"black"}}>
+      <div className="row justify-content-center text-light">
+        <div className="col-md-6 mt-4">
+          <form>
+            <div className="mb-3">
+              <label htmlFor="name" className="form-label">
+                <ion-icon icon={person} className="me-2"></ion-icon>
+                 <span> Name</span>
+              </label>
+              <input type="text" className="form-control" id="name" name="name" value={name}  onChange={e=>setName(e.target.value)}/>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="email" className="form-label">
+                <ion-icon icon={mail} className="me-2"></ion-icon>
+                <span> Email</span>
+              </label>
+              <input type="email" className="form-control" id="email" name="email"  value={email} onChange={e=>setEmail(e.target.value)} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="mobile" className="form-label">
+                <ion-icon icon={call} className="me-2"></ion-icon>
+                <span> Mobile Number</span>
+              </label>
+              <input type="tel" className="form-control" id="mobile" name="mobile"  value={mobileNumber} onChange={e=>setMobileNumber(e.target.value)} />
+            </div>
+            <div className="mb-3">
+              <label htmlFor="gig" className="form-label">
+              <ion-icon name="ticket-outline"></ion-icon>
+                <span> What's your Gig</span>
+              </label>
+              <textarea className="form-control" id="gig" name="gig" rows="3"  value={gig} onChange={e=>setGig(e.target.value)}></textarea>
+            </div>
+            <div className="mb-3">
+              <label htmlFor="cost" className="form-label">
+                <ion-icon icon={logoUsd} className="me-2"></ion-icon>
+                <span> Cost</span>
+              </label>
+              <input type="number" className="form-control" id="cost" name="cost"  value={cost} onChange={e=>setCost(e.target.value)} />
+            </div>
+            <button type="button" className="btn btn-primary" onClick={handleSubmit}>Submit</button>
+          </form>
+        </div>
+      </div>
+    </div>
+    </div>
+  )
+}
+
+export default App
